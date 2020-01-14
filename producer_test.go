db99088d2bb753e58abf964b379b2f078440d963
David Subiros <subiros.david@gmail.com>
2020-01-14 09:39:07 +0000

    fix test concurrency with a set of independent channels per test

--- producer_test.go
+++ producer_test.go
@@ -2,7 +2,6 @@ package kafka_test
 
 import (
 	"errors"
-	"sync"
 	"testing"
 	"time"
 
@@ -13,43 +12,63 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+// kafka topic that will be used for testing
 const testTopic = "testTopic"
+
+// timeout for test channels message propagation
+const TIMEOUT = 1 * time.Second
 
 // var testBrokers = []string{"1.1.1.1:1111", "2.2.2.2:2222"}
 var testBrokers = []string{"localhost:9092"}
 
-// Sarama channels for testing (i.e. correspond to sarama channels)
-var (
-	saramaErrsChan  = make(chan *sarama.ProducerError)
+// createSaramaChannels creates sarama channels for testing
+func createSaramaChannels() (saramaErrsChan chan *sarama.ProducerError, saramaInputChan chan *sarama.ProducerMessage) {
+	saramaErrsChan = make(chan *sarama.ProducerError)
 	saramaInputChan = make(chan *sarama.ProducerMessage)
-)
+	return
+}
 
-var (
-	chOut    = make(chan []byte)
-	chErr    = make(chan error)
+// createLocalChannels creates local channels for testing
+func createLocalChannels() (chOut chan []byte, chErr chan error, chCloser, chClosed chan struct{}) {
+	chOut = make(chan []byte)
+	chErr = make(chan error)
 	chCloser = make(chan struct{})
 	chClosed = make(chan struct{})
-)
-
-// AsyncProducer ErrorsFunc mock that returns the testing saramaErrsChan channel
-func mockErrorsFunc() <-chan *sarama.ProducerError {
-	return saramaErrsChan
+	return
 }
 
-// AsyncProducer InputFunc mock that returns the testing saramaInputChan channel
-func mockInputFunc() chan<- *sarama.ProducerMessage {
-	return saramaInputChan
+// createMockErrorsFunc returns a mock for AsyncProducer ErrorsFunc with the provided Sarama errors Channel
+func createMockErrorsFunc(saramaErrsChan chan *sarama.ProducerError) func() <-chan *sarama.ProducerError {
+	return func() <-chan *sarama.ProducerError {
+		return saramaErrsChan
+	}
 }
 
-// Mock AsyncProducerwith implemented methods for testing
-var asyncProducer = &mock.AsyncProducerMock{
-	ErrorsFunc: mockErrorsFunc,
-	InputFunc:  mockInputFunc,
+// AsyncProducer InputFunc returns a mock for AsyncProducer InputFunc with the provied Sarama input channel
+func createMockInputFunc(saramaInputChan chan *sarama.ProducerMessage) func() chan<- *sarama.ProducerMessage {
+	return func() chan<- *sarama.ProducerMessage {
+		return saramaInputChan
+	}
 }
 
-// mockNewAsyncProducerEmpty returns an AsyncProducer mock with all the necessary methods implemented for testing
-func mockNewAsyncProducerComplete(addrs []string, conf *sarama.Config) (kafka.AsyncProducer, error) {
-	return asyncProducer, nil
+// createAsyncProducer returns a mock AsyncProducer with the provided Sarama channels
+func createAsyncProducer(
+	saramaErrsChan chan *sarama.ProducerError, saramaInputChan chan *sarama.ProducerMessage) *mock.AsyncProducerMock {
+	var asyncProducer = &mock.AsyncProducerMock{
+		ErrorsFunc: createMockErrorsFunc(saramaErrsChan),
+		InputFunc:  createMockInputFunc(saramaInputChan),
+	}
+	return asyncProducer
+}
+
+// createMockNewAsyncProducerComplete returns a mock NewAsyncProducerFunc that returns
+// a new AsyncProducer with provided Sarama channels
+func createMockNewAsyncProducerComplete(
+	saramaErrsChan chan *sarama.ProducerError, saramaInputChan chan *sarama.ProducerMessage) func(
+	addrs []string, conf *sarama.Config) (kafka.AsyncProducer, error) {
+	return func(addrs []string, conf *sarama.Config) (kafka.AsyncProducer, error) {
+		return createAsyncProducer(saramaErrsChan, saramaInputChan), nil
+	}
 }
 
 // mockNewAsyncProducerEmpty returns an AsyncProducer mock with no methods implemented
@@ -58,14 +77,30 @@ func mockNewAsyncProducerEmpty(addrs []string, conf *sarama.Config) (kafka.Async
 	return &mock.AsyncProducerMock{}, nil
 }
 
-// Test wrong Producer creation because of channels not provided by caller
+// GetFromSaramaChans select Sarama channels, and return whichever is triggered (only one value per call).
+// If none is triggered after timeout, timeout will be triggered
+func GetFromSaramaChans(
+	saramaErrsChan chan *sarama.ProducerError, saramaInputChan chan *sarama.ProducerMessage) (
+	input *sarama.ProducerMessage, err *sarama.ProducerError, timeout bool) {
+	select {
+	case input := <-saramaInputChan:
+		return input, nil, false
+	case err := <-saramaErrsChan:
+		return nil, err, false
+	case <-time.After(TIMEOUT):
+		return nil, nil, true
+	}
+}
+
+// TestProducerMissingChannels tests wrong producer creation because of channels not provided by caller
 func TestProducerMissingChannels(t *testing.T) {
+
 	Convey("Given the intention to initialize a kafka Producer", t, func() {
+
 		saramaCli := &mock.SaramaMock{
 			NewAsyncProducerFunc: mockNewAsyncProducerEmpty,
 		}
-		// outputChan, errorsChan := make(chan []byte), make(chan error)
-		// closerChan, closedChan := make(chan struct{}), make(chan struct{})
+		chOut, chErr, chCloser, chClosed := createLocalChannels()
 		Convey("Missing outputChan will cause ErrNoOputputChannel", func() {
 			producer, err := kafka.NewProducerWithSaramaClient(testBrokers, testTopic, 123,
 				nil, chErr, chCloser, chClosed, saramaCli)
@@ -97,185 +132,82 @@ func TestProducerMissingChannels(t *testing.T) {
 	})
 }
 
-// GetFromSaramaChans select Sarama channels, and return whichever is triggered (only one value per call).
-// No default, to prevent race conditions, but timeout defined to prevent long tests.
-func GetFromSaramaChans() (input *sarama.ProducerMessage, err *sarama.ProducerError, timeout bool) {
-	select {
-	case input := <-saramaInputChan:
-		return input, nil, false
-	case err := <-saramaErrsChan:
-		return nil, err, false
-	case <-time.After(1 * time.Second):
-		return nil, nil, true
-	}
-}
-
-// GetFromLocalChans selects provided local channels, and return whichever is triggered (only one value per call).
-// No default, to prevent race conditions.
-func GetFromLocalChans(chOut chan []byte, chErr chan error, chCloser, chClosed chan struct{}) (out []byte, err error, closer, closed *struct{}, timeout bool) {
-	select {
-	case out := <-chOut:
-		return out, nil, nil, nil, false
-	case err := <-chErr:
-		return nil, err, nil, nil, false
-	case closer := <-chCloser:
-		return nil, nil, &closer, nil, false
-	case closed := <-chClosed:
-		return nil, nil, nil, &closed, false
-	case <-time.After(1 * time.Second):
-		return nil, nil, nil, nil, true
-	}
-}
-
-// TestSendMessage validates that messages sent to the local output channel are correctly forwarded to Sarama Input channel
-func TestSendMessage(t *testing.T) {
+// TestProducerChannels tests that messages, errors, and closing events are correctly directed to the expected channels
+func TestProducerChannels(t *testing.T) {
 
 	Convey("Given a correct initialization of a Kafka Producer", t, func() {
+		chSaramaErr, chSaramaIn := createSaramaChannels()
 		saramaCli := &mock.SaramaMock{
-			NewAsyncProducerFunc: mockNewAsyncProducerComplete,
+			NewAsyncProducerFunc: createMockNewAsyncProducerComplete(chSaramaErr, chSaramaIn),
 		}
-		// chOut, chErr := make(chan []byte), make(chan error)
-		// chCloser, chClosed := make(chan struct{}), make(chan struct{})
+		chOut, chErr, chCloser, chClosed := createLocalChannels()
 		producer, err := kafka.NewProducerWithSaramaClient(testBrokers, testTopic, 123,
 			chOut, chErr, chCloser, chClosed, saramaCli)
 
-		Convey("Messages from output channel are redirected to sarama AsyncProducer", func() {
-
+		Convey("Producer is correctly created without error", func() {
 			// Validate proudcer correctly created
 			So(producer, ShouldNotBeNil)
 			So(err, ShouldBeNil)
 			So(len(saramaCli.NewAsyncProducerCalls()), ShouldEqual, 1)
+		})
 
-			// Prepare concurrent validtion of Sarama channels with waitGroup
-			var (
-				saramaIn  *sarama.ProducerMessage = nil
-				saramaErr *sarama.ProducerError   = nil
-				timeout   bool                    = false
-			)
-			var wg sync.WaitGroup
-			wg.Add(1)
-			go func() {
-				saramaIn, saramaErr, timeout = GetFromSaramaChans()
-				wg.Done()
-			}()
-
-			// Send message to local kafka output chan, and wait for WaitGroup to finish
+		Convey("Messages from the caller's output channel are redirected to Sarama AsyncProducer", func() {
+			// Send message to local kafka output chan
 			message := "HELLO"
 			msg := kafkatest.NewMessage([]byte(message))
 			chOut <- msg.GetData()
-			wg.Wait()
-
-			// Validate that message was received in local message chain, and nothing else
+			// Read sarama channels with timeout
+			saramaIn, saramaErr, timeout := GetFromSaramaChans(chSaramaErr, chSaramaIn)
+			// Validate that message was received by sarama message chain, with no error.
 			So(timeout, ShouldBeFalse)
 			So(saramaErr, ShouldBeNil)
 			So(saramaIn.Topic, ShouldEqual, testTopic)
 			So(saramaIn.Value, ShouldEqual, message)
 		})
-	})
-}
 
-// TestReceiveError validates that errors comming from Sarama error channel are correctly forwarded to the local error channel
-func TestReceiveError(t *testing.T) {
-
-	Convey("Given a correct initialization of a kafka Producer", t, func() {
-		saramaCli := &mock.SaramaMock{
-			NewAsyncProducerFunc: mockNewAsyncProducerComplete,
-		}
-		producer, err := kafka.NewProducerWithSaramaClient(testBrokers, testTopic, 123,
-			chOut, chErr, chCloser, chClosed, saramaCli)
-
-		Convey("Errors from sarama AsyncProducer are redirected to the errors channel", func() {
-
-			// Validate proudcer correctly created
-			So(producer, ShouldNotBeNil)
-			So(err, ShouldBeNil)
-			So(len(saramaCli.NewAsyncProducerCalls()), ShouldEqual, 1)
-
-			// Prepare concurrent validtion of local channels with waitGroup
-			var (
-				localOut    []byte    = nil
-				localErr    error     = nil
-				localCloser *struct{} = nil
-				localClosed *struct{} = nil
-				timeout     bool      = false
-			)
-			var wg sync.WaitGroup
-			wg.Add(1)
-			go func() {
-				localOut, localErr, localCloser, localClosed, timeout = GetFromLocalChans(chOut, chErr, chCloser, chClosed)
-				wg.Done()
-			}()
-
-			// Send Error to Sarama channel, and wait for wait goup to finish
+		Convey("Errors from Sarama AsyncProducer are redirected to the caller's errors channel", func() {
+			// Send error to Sarama channel
 			producerError := &sarama.ProducerError{
 				Msg: &sarama.ProducerMessage{
 					Topic: testTopic,
 				},
 				Err: errors.New("error text"),
 			}
-			saramaErrsChan <- producerError
-			wg.Wait()
-
-			// Validate that error was received in local error chain, and nothing else
+			chSaramaErr <- producerError
+			// Read local error channel with timeout
+			var (
+				localErr error = nil
+				timeout  bool  = false
+			)
+			select {
+			case e := <-chErr:
+				localErr = e
+			case <-time.After(TIMEOUT):
+				timeout = true
+			}
+			// Validate that error was received in local error chain
 			So(timeout, ShouldBeFalse)
-			So(localOut, ShouldBeNil)
-			So(localCloser, ShouldBeNil)
-			So(localClosed, ShouldBeNil)
 			So(localErr, ShouldNotBeNil)
 			So(localErr, ShouldResemble, producerError)
 		})
-		Convey("Closer channel closes the client correctly", func() {
-		})
-	})
-}
 
-func TestCloseKafka(t *testing.T) {
-
-	Convey("Given a correct initialization of a kafka Producer", t, func() {
-		saramaCli := &mock.SaramaMock{
-			NewAsyncProducerFunc: mockNewAsyncProducerComplete,
-		}
-		// chOut, chErr := make(chan []byte), make(chan error)
-		// chCloser, chClosed := make(chan struct{}), make(chan struct{})
-		producer, err := kafka.NewProducerWithSaramaClient(testBrokers, testTopic, 123,
-			chOut, chErr, chCloser, chClosed, saramaCli)
-
-		Convey("Errors from sarama AsyncProducer are redirected to the errors channel", func() {
-
-			// Validate proudcer correctly created
-			So(producer, ShouldNotBeNil)
-			So(err, ShouldBeNil)
-			So(len(saramaCli.NewAsyncProducerCalls()), ShouldEqual, 1)
-
-			// Prepare concurrent validtion of local channels with waitGroup
+		Convey("closing local closer's channel causes kafka producer to close the closed channel", func() {
+			close(chCloser)
+			// Read local closed channel with timeout
 			var (
-				localOut    []byte    = nil
-				localErr    error     = nil
-				localCloser *struct{} = nil
-				localClosed *struct{} = nil
-				timeout     bool      = false
+				localClosed bool = false
+				timeout     bool = false
 			)
-			var wg sync.WaitGroup
-			wg.Add(1)
-			go func() {
-				localOut, localErr, localCloser, localClosed, timeout = GetFromLocalChans(chOut, chErr, chCloser, chClosed)
-				wg.Done()
-			}()
-
-			// Send data to local Closer channel, and wait for wait goup to finish
-			type closerStruct struct{}
-			closerVar := closerStruct{}
-			chCloser <- closerVar
-			wg.Wait()
-
-			// Validate that error was received in local error chain, and nothing else
+			select {
+			case <-chClosed:
+				localClosed = true
+			case <-time.After(TIMEOUT):
+				timeout = true
+			}
+			// Validate that kafka closed correctly
 			So(timeout, ShouldBeFalse)
-			So(localOut, ShouldBeNil)
-			So(localCloser, ShouldNotBeNil)
-			So(localClosed, ShouldBeNil)
-			So(localErr, ShouldBeNil)
+			So(localClosed, ShouldBeTrue)
 		})
-		Convey("Closer channel closes the client correctly", func() {
-		})
+
 	})
 }
